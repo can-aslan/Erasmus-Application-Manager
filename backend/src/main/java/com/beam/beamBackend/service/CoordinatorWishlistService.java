cc92e1684e6a1b180ac3b4f99962ef1e19a30658
KkubilayY <kemalkubilayyilmaz@gmail.com>
2022-12-05 01:09:19 +0300

    feat(ERASMUS-152)

---
+++ backend/src/main/java/com/beam/beamBackend/service/CoordinatorWishlistService.java
@@ -0,0 +1,28 @@
+package com.beam.beamBackend.service;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.beam.beamBackend.model.Course;
+import com.beam.beamBackend.model.CourseWishlist;
+import com.beam.beamBackend.repository.CoordinatorWishlistRepository;
+
+@Service
+public class CoordinatorWishlistService {
+    private final CoordinatorWishlistRepository coordinatorWishlistRepository;
+
+    @Autowired
+    public CoordinatorWishlistService( CoordinatorWishlistRepository coordinatorWishlistRepository){
+        this.coordinatorWishlistRepository = coordinatorWishlistRepository;
+    }
+
+    public ArrayList<CourseWishlist> getWaitingWishlist() {
+        return coordinatorWishlistRepository.findWishlist();
+    }
+
+    public ArrayList<Course> viewWishList(long studentId) {
+        return coordinatorWishlistRepository.viewWishList(studentId);
+    }
+
+}
