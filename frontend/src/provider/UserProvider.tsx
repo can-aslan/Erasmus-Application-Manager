f5fc9103a04add31919f8701ec53235a85f07e5d
Selim Can Guler <selimcangler@gmail.com>
2022-12-05 00:16:24 +0300

    chore(ERASMUS-149): added persistent login support

--- frontend/src/provider/UserProvider.tsx
+++ frontend/src/provider/UserProvider.tsx
@@ -3,6 +3,7 @@ import { User } from "../types";
 
 interface UserContextProps {
     user: User | {},
+    isUser: (x: any) => x is User,
     setUser: React.Dispatch<React.SetStateAction<User | {}>>
 }
 
@@ -14,9 +15,10 @@ const UserContext = createContext<UserContextProps>(null!)
 
 export const UserProvider = ({ children }: UserProviderProps) => {
     const [user, setUser] = useState<User | {}>({})
+    const isUser = (x: any): x is User => Object.keys(x).length !== 0
 
     return (
-        <UserContext.Provider value={{user, setUser}}>
+        <UserContext.Provider value={{user, setUser, isUser}}>
             {children}
         </UserContext.Provider>
     )
