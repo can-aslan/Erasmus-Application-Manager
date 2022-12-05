9bb1d22857e83e0124caf2927da0557dd31c3fe8
Elif <elif.kervan@ug.bilkent.edu.tr>
2022-12-05 22:25:12 +0300

    feat(ERASMUS-135): Account Controller

--- backend/src/main/java/com/beam/beamBackend/model/User.java
+++ backend/src/main/java/com/beam/beamBackend/model/User.java
@@ -1,13 +1,34 @@
 package com.beam.beamBackend.model;
 
-import com.beam.beamBackend.enums.UserType;
+import java.util.UUID;
 
-public abstract class User{
-    long id;
+import com.beam.beamBackend.enums.UserType;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class User{
+    
+    UUID id;
     String name;
     String surname;
     String email;
     long bilkentId;
     String password;
     UserType userType;
+
+    public User(@JsonProperty("id") UUID id,
+                @JsonProperty("name") String name,
+                @JsonProperty("surname") String surname,
+                @JsonProperty("email") String email,
+                @JsonProperty("bilkentId") long bilkentId,
+                @JsonProperty("password") String password,
+                @JsonProperty("userType") UserType userType) {
+
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.bilkentId = bilkentId;
+        this.password = password;
+        this.userType = userType;
+    }
 }
