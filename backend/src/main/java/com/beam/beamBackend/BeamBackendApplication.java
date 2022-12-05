87320d7035b11bf2cccb11a1e2ebed5f711a0c7c
Elif <elif.kervan@ug.bilkent.edu.tr>
2022-12-05 23:59:16 +0300

    feat(ERASMUS-136)

--- backend/src/main/java/com/beam/beamBackend/BeamBackendApplication.java
+++ backend/src/main/java/com/beam/beamBackend/BeamBackendApplication.java
@@ -2,13 +2,28 @@ package com.beam.beamBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@R
 public class BeamBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeamBackendApplication.class, args);
 		System.out.println("yubii");
 	}
+
+	@GetMapping()
+    public ResponseEntity<String> helo() {
+        System.out.println("mal");
+        try {
+            System.out.println("olleyyyy");
+            return ResponseEntity.ok("test basairli merhabe dunya!!");
+        } catch (Exception e) {
+            return ResponseEntity.ok("HttpStatus.FOUND"); // might change later
+        }        
+    }
 
 }
