66fee1c231ca9f4d3f52e64970e036d68541a657
Yağız Can Aslan <yagizcanaslan0@gmail.com>
2022-12-05 16:15:10 +0300

    feat(ERASMUS-141): Started implementation of test classes

---
+++ backend/src/test/java/com/beam/beamBackend/controller/StudentCourseRequestControllerTests.java
@@ -0,0 +1,16 @@
+package com.beam.beamBackend.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.beam.beamBackend.enums.StudentType;
+
+@WebMvcTest(controllers = StudentCourseRequestController.class)
+@ContextConfiguration(classes = StudentCourseRequestController.class)
+@WithMockUser(roles = {"OUTGOING"}) // Temporary user role for outgoing student
+public class StudentCourseRequestControllerTests extends ControllerTestsSetup {
+    @MockBean
+    private IStudentCourseRequestService = studentCourseRequestService;
+}
