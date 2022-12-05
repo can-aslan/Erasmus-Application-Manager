602c463d1cac032e214188f449be93d35bae6f02
Yağız Can Aslan <86242974+can-aslan@users.noreply.github.com>
2022-12-06 00:55:22 +0300

    chore(ERASMUS-141): Very small Qualifier changes

--- backend/src/main/java/com/beam/beamBackend/repository/StudentRepository.java
+++ backend/src/main/java/com/beam/beamBackend/repository/StudentRepository.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 import com.beam.beamBackend.model.Student;
 
+@Qualifier("student")
 @Repository
 public interface StudentRepository
 //extends JpaRepository<Student, Long>
