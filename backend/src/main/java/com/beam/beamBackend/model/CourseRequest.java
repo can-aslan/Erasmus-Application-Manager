795b50e536e87e2c62d225c79edf9092d9cd3ce3
Yağız Can Aslan <86242974+can-aslan@users.noreply.github.com>
2022-12-06 22:24:04 +0300

    feat(ERASMUS-141): Added lombok

---
+++ backend/src/main/java/com/beam/beamBackend/model/CourseRequest.java
@@ -0,0 +1,15 @@
+package com.beam.beamBackend.model;
+
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CourseRequest {
+    
+}
