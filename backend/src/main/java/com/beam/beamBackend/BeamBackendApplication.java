e01de2d1a070e18ce4bf4318212bcddfb786d0d6
Elif <elif.kervan@ug.bilkent.edu.tr>
2022-12-06 21:47:47 +0300

    chore(ERASMUS-135): Edit main

--- backend/src/main/java/com/beam/beamBackend/BeamBackendApplication.java
+++ backend/src/main/java/com/beam/beamBackend/BeamBackendApplication.java
@@ -12,18 +12,5 @@ public class BeamBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeamBackendApplication.class, args);
-		System.out.println("yubii");
 	}
-
-	@GetMapping()
-    public ResponseEntity<String> helo() {
-        System.out.println("mal");
-        try {
-            System.out.println("olleyyyy");
-            return ResponseEntity.ok("test basairli merhabe dunya!!");
-        } catch (Exception e) {
-            return ResponseEntity.ok("HttpStatus.FOUND"); // might change later
-        }        
-    }
-
 }
