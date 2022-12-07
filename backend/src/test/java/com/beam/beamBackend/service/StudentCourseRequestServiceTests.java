1bfe7061bafeca91862fbe44839dd288b58e176b
Yağız Can Aslan <86242974+can-aslan@users.noreply.github.com>
2022-12-07 21:41:31 +0300

    feat(ERASMUS-142): Added another StudentCourseRequestServiceTests test case and fixed test exception issues.

--- backend/src/test/java/com/beam/beamBackend/service/StudentCourseRequestServiceTests.java
+++ backend/src/test/java/com/beam/beamBackend/service/StudentCourseRequestServiceTests.java
@@ -1,15 +1,18 @@
 package com.beam.beamBackend.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.junit.jupiter.MockitoExtension;
 import static org.assertj.core.api.Assertions.assertThat;
 import com.beam.beamBackend.model.CourseRequest;
 import com.beam.beamBackend.repository.ICourseRequestRepository;
 
+@ExtendWith(MockitoExtension.class)
 public class StudentCourseRequestServiceTests {
-    @MockBean
+    @Mock
     private ICourseRequestRepository courseRequestRepository;
 
     @InjectMocks
@@ -29,5 +32,21 @@ public class StudentCourseRequestServiceTests {
         Mockito.when(courseRequestRepository.saveRequest(courseRequest)).thenReturn(true);
         boolean result = studentCourseRequestService.requestCourse(courseRequest);
         assertThat(result).isTrue();
+    }
+
+    @Test
+    public void requestInvalidCourse() throws Exception {
+        final CourseRequest courseRequest = new CourseRequest(
+            null,
+            (long) 22001943,
+            "WRONG_COURSE_INFO",
+            "Other_Course",
+            "CS-319",
+            "www.othercourselink.com"
+        );
+
+        Mockito.when(courseRequestRepository.saveRequest(courseRequest)).thenReturn(false);
+        boolean result = studentCourseRequestService.requestCourse(courseRequest);
+        assertThat(result).isFalse();
     }
 }
