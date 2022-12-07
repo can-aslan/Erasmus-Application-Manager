eab6a7bd3d9fbe53073c175bf3142c9e873fe12d
Yağız Can Aslan <86242974+can-aslan@users.noreply.github.com>
2022-12-07 21:23:17 +0300

    chore(ERASMUS-142): Modified test classes by removing unused imports and changing method names

--- backend/src/test/java/com/beam/beamBackend/service/StudentCourseRequestServiceTests.java
+++ backend/src/test/java/com/beam/beamBackend/service/StudentCourseRequestServiceTests.java
@@ -2,27 +2,18 @@ package com.beam.beamBackend.service;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
-
 import static org.assertj.core.api.Assertions.assertThat;
-
-import com.beam.beamBackend.service.IStudentCourseRequestService;
-import com.beam.beamBackend.service.StudentCourseRequestService;
-
-import lombok.RequiredArgsConstructor;
-
 import com.beam.beamBackend.model.CourseRequest;
 import com.beam.beamBackend.repository.ICourseRequestRepository;
 
 public class StudentCourseRequestServiceTests {
-    @Autowired
-    private StudentCourseRequestService studentCourseRequestService;
-
     @MockBean
     private ICourseRequestRepository courseRequestRepository;
+
+    @InjectMocks
+    private StudentCourseRequestService studentCourseRequestService;
 
     @Test
     public void requestValidCourse() throws Exception {
@@ -35,6 +26,7 @@ public class StudentCourseRequestServiceTests {
             "www.othercourselink.com"
         );
 
+        Mockito.when(courseRequestRepository.saveRequest(courseRequest)).thenReturn(true);
         boolean result = studentCourseRequestService.requestCourse(courseRequest);
         assertThat(result).isTrue();
     }
