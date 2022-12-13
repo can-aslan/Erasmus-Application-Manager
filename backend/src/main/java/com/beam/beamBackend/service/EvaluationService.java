b952883438abe704efeaa17a35af53b6b2df837d
Elif <elif.kervan@ug.bilkent.edu.tr>
2022-12-13 21:20:57 +0300

    feat(ERASMUS-179): Added evaluation endpoints

--- backend/src/main/java/com/beam/beamBackend/service/EvaluationService.java
+++ backend/src/main/java/com/beam/beamBackend/service/EvaluationService.java
@@ -1,6 +1,7 @@
 package com.beam.beamBackend.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
@@ -9,8 +10,10 @@ import com.beam.beamBackend.enums.EvalStatus;
 import com.beam.beamBackend.model.CourseEvaluationForm;
 import com.beam.beamBackend.model.EvaluationForm;
 import com.beam.beamBackend.model.UniEvaluationForm;
-import com.beam.beamBackend.repository.AccountRepository;
 import com.beam.beamBackend.repository.CourseEvaluationRepository;
+import com.beam.beamBackend.repository.IAccountRepository;
+import com.beam.beamBackend.repository.ICourseEvalRepository;
+import com.beam.beamBackend.repository.IUniversityEvalRepository;
 import com.beam.beamBackend.repository.UniEvaluationRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -18,30 +21,32 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class EvaluationService {
 
-    private final UniEvaluationRepository uniEvalRepo;
-    private final CourseEvaluationRepository courseEvalRepo;
-    private final AccountRepository userRepo;
+    private final IUniversityEvalRepository uniEvalRepo;
+    private final ICourseEvalRepository courseEvalRepo;
+    private final IAccountRepository userRepo;
 
     public UUID evaluateUni(UniEvaluationForm uniEval) throws Exception {
         try {
+            System.out.println(uniEval);
             // throw exception if user is not found in the db
             if (!userRepo.existsByBilkentId(uniEval.getAuthorId())) {
                 throw new Exception("user not found");
             }
 
-            EvalStatus evalStatus = uniEvalRepo.findEvalStatusByAuthorId(uniEval.getAuthorId());
+            Optional<UniEvaluationForm> uniEvalDB = uniEvalRepo.findEvalByAuthorId(uniEval.getAuthorId());
 
-            if (evalStatus == null) {
+            System.out.println(uniEval);
+            System.out.println(uniEvalDB);
+            if (!uniEvalDB.isPresent()) {
                 uniEval.setId(UUID.randomUUID());
-                Boolean result = uniEvalRepo.save(uniEval);
-            } else if (evalStatus == EvalStatus.SUBMITTED) {
+                uniEvalRepo.save(uniEval);
+            } else if (uniEvalDB.get().getEvalStatus() == EvalStatus.SUBMITTED) {
                 throw new Exception("uni eval already exists for this author");
             } else {
-                // update current evaluation
+                uniEvalRepo.updateEval(uniEval.getComment(), uniEval.getRate(), uniEval.getEvalStatus(), uniEval.getAuthorId());
             }
 
-            // this will be inaccurate for update !!!!!
-            return uniEval.getUniId();
+            return uniEval.getUniId(); //change!!!
             
         } catch (Exception e) {
             System.out.println("course eval exception");
@@ -68,16 +73,15 @@ public class EvaluationService {
                 throw new Exception("user not found");
             }
 
-            EvalStatus evalStatus = null;
-            // courseEvalRepo.findEvalStatusByAuthorId(courseEval.getAuthorId());
+            Optional<CourseEvaluationForm> courseEvalDB = courseEvalRepo.findEvalByAuthorId(courseEval.getAuthorId());
 
-            if (evalStatus == null) {
+            if (!courseEvalDB.isPresent()) {
                 courseEval.setId(UUID.randomUUID());
-                Boolean result = courseEvalRepo.save(courseEval);
-            } else if (evalStatus == EvalStatus.SAVED) {
+                courseEvalRepo.save(courseEval);
+            } else if (courseEvalDB.get().getEvalStatus() == EvalStatus.SUBMITTED) {
                 throw new Exception("uni eval already exists for this author");
             } else {
-                // update current evaluation
+                courseEvalRepo.updateEval(courseEval.getComment(), courseEval.getRate(), courseEval.getEvalStatus(), courseEval.getAuthorId());
             }
 
             // this will be inaccurate for update !!!!!
@@ -94,9 +98,10 @@ public class EvaluationService {
         try {
             List<CourseEvaluationForm> result = courseEvalRepo.findByCourseId(courseId);
 
-            if (result == null) {
+            if (result == null || result.size() == 0) {
                 throw new Exception("course not found or there is no eval"); //cahnge maybe
             }
+
             return result;            
         } catch (Exception e) {
             System.out.println("cannot get eval exception");
@@ -105,55 +110,47 @@ public class EvaluationService {
         }
     }
 
-    public List<CourseEvaluationForm> getAllCourseEval(UUID uniId) throws Exception {
-        try {
-
-            // check if uni id exists
-
-            //
-            List<CourseEvaluationForm> result = courseEvalRepo.findByUniId(uniId);
-
-            if (result == null) {
-                throw new Exception("no eval is found"); //maybe not error?
-            }
-            return result;            
-        } catch (Exception e) {
-            System.out.println("cannot get eval exception");
-            e.printStackTrace();
-            throw e;
-        }
-    }
-
-    public EvaluationForm getSavedUniEval(long authorId) throws Exception {
+    public UniEvaluationForm getStudentUniEval(Long authorId, EvalStatus evalStatus) throws Exception {
         try {
             // throw exception if user is not found in the db
             if (!userRepo.existsByBilkentId(authorId)) {
                 throw new Exception("user not found");
             }
 
-            EvalStatus evalStatus = uniEvalRepo.findEvalStatusByAuthorId(authorId);
+            Optional<UniEvaluationForm> form = uniEvalRepo.findEvalByAuthorIdAndEvalStatus(authorId, evalStatus);
 
-            if (evalStatus == null) {
-                // empty evaluation
-            } else if (evalStatus == EvalStatus.SUBMITTED) {
-                throw new Exception("uni eval already exists for this author");
-            } else {
-                // get saved eval
-            }
+            if (!form.isPresent()) {
+                return new UniEvaluationForm(null, authorId, 0.0, "", EvalStatus.EMPTY, null);
+            } 
 
-            // this will be inaccurate for update !!!!!
-            return null;//here
-            
+            return form.get();            
+
         } catch (Exception e) {
             System.out.println("course eval exception");
             e.printStackTrace();
             throw e;
         }
     }
-
-    // public EvaluationForm getSavedCourseEval(long authorId, UUID courseId) {
-
-    // }
-
     
+    public CourseEvaluationForm getStudentCourseEval(Long authorId, EvalStatus evalStatus) throws Exception {
+        try {
+            // throw exception if user is not found in the db
+            if (!userRepo.existsByBilkentId(authorId)) {
+                throw new Exception("user not found");
+            }
+
+            Optional<CourseEvaluationForm> form = courseEvalRepo.findEvalByAuthorIdAndEvalStatus(authorId, evalStatus);
+
+            if (!form.isPresent()) {
+                return new CourseEvaluationForm(null, authorId, 0.0, "", EvalStatus.EMPTY, null);
+            }
+
+            return form.get();
+
+        } catch (Exception e) {
+            System.out.println("course eval exception");
+            e.printStackTrace();
+            throw e;
+        }
+    }
 }
