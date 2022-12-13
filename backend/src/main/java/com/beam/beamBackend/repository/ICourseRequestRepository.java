00e5855aab72a6c07df099630a05370683a0a96a
Yağız Can Aslan <86242974+can-aslan@users.noreply.github.com>
2022-12-13 14:20:24 +0300

    feat(ERASMUS-176): Model implementations of CourseWishlist, CourseWishlistItem and ICourseWishlistRepository implementation

--- backend/src/main/java/com/beam/beamBackend/repository/ICourseRequestRepository.java
+++ backend/src/main/java/com/beam/beamBackend/repository/ICourseRequestRepository.java
@@ -2,7 +2,6 @@ package com.beam.beamBackend.repository;
 
 import java.util.List;
 import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
