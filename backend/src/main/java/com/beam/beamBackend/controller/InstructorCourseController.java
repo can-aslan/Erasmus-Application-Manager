44dbec57263efed63525fb7df6e00957555af903
Elif <elif.kervan@ug.bilkent.edu.tr>
2022-12-18 03:58:10 +0300

    fix(ERASMUS-298):Updated endpoint url

--- backend/src/main/java/com/beam/beamBackend/controller/InstructorCourseController.java
+++ backend/src/main/java/com/beam/beamBackend/controller/InstructorCourseController.java
@@ -43,7 +43,7 @@ public class InstructorCourseController {
     }
 
     @CrossOrigin(origins = "http://localhost:5173", allowedHeaders = "*", allowCredentials = "true")
-    @GetMapping("/course/{instructorId}")
+    @GetMapping("{instructorId}/course")
     public ResponseEntity<Object> getInstructorCourses(@Valid @PathVariable("instructorId") UUID instructorId) {
         try {
             List<BilkentCourse> courseList = instructorService.getInstructorCourses(instructorId);
